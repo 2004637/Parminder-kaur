13cec9ac5aa4a4e78ba952c1740809d272df7207
unknown <k.parminder.5488@gmail.com>
2022-07-19 11:50:23 +0530

    first commit

---
+++ day 2.py
@@ -0,0 +1,18 @@
+string=input(("enter a string"))
+if(string==string[::-1])
+   print("The string is palindrome")
+else:
+    print("Not a palindrome")
+
+
+a=10
+b=4
+def add():
+    return a+b
+def nu1():
+    d=add()
+    c=a*d
+    return(c)
+print(nu1())
+
+    
